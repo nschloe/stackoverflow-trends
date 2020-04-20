9bd064461fc82607f5177df348a0f644b80826d8
Nico Schlömer <nico.schloemer@gmail.com>
2020-04-20 17:57:54 +0200

    add some missing files

--- data-stackoverflow/plot.py
+++ data-stackoverflow/plot.py
@@ -21,23 +21,23 @@ for group_name, group in data.items():
     # sort them such that the largest at the last time step gets plotted first and the
     # colors are in a nice order
     last_vals = []
-    for repo in group:
-        filename = this_dir / "data" / (repo.replace("/", "_") + ".json")
+    for tag in group:
+        filename = this_dir / "data" / (tag + ".json")
         with open(filename) as f:
             content = json.load(f)
         last_vals.append(list(content["data"].values())[-1])
     group = [group[i] for i in _argsort(last_vals)[::-1]]
 
-    filenames = [
-        this_dir / "data" / (repo.replace("/", "_") + ".json") for repo in group
-    ]
+    filenames = [this_dir / "data" / (tag + ".json") for tag in group]
 
-    hotware.plot(filenames)
-    plt.title("Number of GitHub stars", fontsize=14)
+    # hotware.plot(filenames)
+    # plt.title("Number of tags on StackOverflow", fontsize=14)
 
-    # hotware.plot_per_day(filenames)
-    # plt.title("Daily number of GitHub stars", fontsize=14)
+    hotware.plot_per_day(filenames)
+    plt.title("Daily number of questions on StackOverflow", fontsize=14)
 
+    # plt.title("% of all questions that month", fontsize=14)
     plt.show()
+    # exit(1)
     # plt.savefig(group_name + ".svg", transparent=True, bbox_inches="tight")
     # plt.close()
---
+++ data-stackoverflow/update.py
@@ -0,0 +1,25 @@
+import json
+import pathlib
+
+import hotware
+
+
+def update_groups():
+    this_dir = pathlib.Path(__file__).resolve().parent
+    with open(this_dir / "groups.json") as f:
+        data = json.load(f)
+
+    for group in data.values():
+        print()
+        for tag in group:
+            print(tag, "...")
+            hotware.stackoverflow.update_file(
+                this_dir / "data" / (tag + ".json"),
+                tag=tag,
+                license="CC BY",
+                creator="Nico Schlömer",
+            )
+
+
+if __name__ == "__main__":
+    update_groups()
