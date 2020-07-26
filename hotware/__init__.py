bab70f28a67f9ed34821a50d24062c2ffd26f196
Nico Schlömer <nico.schloemer@gmail.com>
2020-07-26 11:02:40 +0200

    test fix

--- hotware/__init__.py
+++ hotware/__init__.py
@@ -1,5 +1,5 @@
-from . import github, stackoverflow
+from . import stackoverflow
 from .__about__ import __version__
 from .tools import plot, plot_per_day
 
-__all__ = ["__version__", "github", "stackoverflow", "plot", "plot_per_day"]
+__all__ = ["__version__", "stackoverflow", "plot", "plot_per_day"]
