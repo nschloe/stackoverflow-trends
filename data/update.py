ed6d2d31a1db6cd51633c1c8c31b65348b6a663a
Nico Schlömer <nico.schloemer@gmail.com>
2021-08-05 08:54:15 +0200

    better update

--- data/update.py
+++ data/update.py
@@ -6,34 +6,25 @@ from rich.progress import Progress
 import stacktags
 
 
-def _string_fixed_length(string, length):
-    if len(string) > length:
-        return string[:length]
-    return string.rjust(length)
-
-
 def update_groups():
     this_dir = pathlib.Path(__file__).resolve().parent
     with open(this_dir / "groups.json") as f:
         data = json.load(f)
 
-    with Progress() as progress:
-        task1 = progress.add_task("group", total=len(data))
-        task2 = progress.add_task("tag", total=0)
+    tags = sorted(set([tag for group in data.values() for tag in group]))
 
-        for group_name, tags in data.items():
-            progress.update(task1, description=_string_fixed_length(group_name, 20))
-            progress.update(task2, total=len(tags), completed=0)
-            for tag in tags:
-                progress.update(task2, description=_string_fixed_length(tag, 20))
-                stacktags.stackoverflow.update_file(
-                    this_dir / "data" / (tag + ".json"),
-                    tag=tag,
-                    license="CC BY",
-                    creator="Nico Schlömer",
-                )
-                progress.advance(task2)
-            progress.advance(task1)
+    with Progress() as progress:
+        task = progress.add_task("tag", total=len(tags))
+
+        for tag in tags:
+            progress.update(task, description=tag)
+            stacktags.stackoverflow.update_file(
+                this_dir / "data" / (tag + ".json"),
+                tag=tag,
+                license="CC BY",
+                creator="Nico Schlömer",
+            )
+            progress.advance(task)
 
 
 if __name__ == "__main__":
