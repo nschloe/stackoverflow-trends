9c7cb29bd0f3cc66b819eff7378bc043a974f76e
Nico Schlömer <nico.schloemer@gmail.com>
2021-12-17 21:39:32 +0100

    not everything that can be a python package should be

--- src/sotrends/__init__.py
+++
@@ -1,4 +0,0 @@
-from ._main import fetch_data
-from ._plot import plot_per_day
-
-__all__ = ["fetch_data", "plot_per_day"]
--- tests/test_basic.py
+++
@@ -1,7 +0,0 @@
-import sotrends
-
-
-def test_basic():
-    data = sotrends.fetch_data(["c++"])
-    plt = sotrends.plot_per_day(data)
-    plt.show()
